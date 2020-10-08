d506714fe960c8f3e561f0e2d7dbc104cfb8d7ca
ritz1804 <59580813+ritz1804@users.noreply.github.com>
2020-10-08 10:16:16 +0530

    Update web_scraper.py

--- web_scraper.py
+++ web_scraper.py
@@ -41,7 +41,7 @@ URL = 'https://www.amazon.in/Apple-iPhone-XR-128GB-Black/dp/B07JG7DS1T/ref=sr_1_
 
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'}
 
-#Function to check wheather the price of the product has dropped less than or equal to our budget
+#Function to check whether the price of the product has dropped less than or equal to our budget
 def check_price():
     page = requests.get(URL, headers = headers)
     soup = BeautifulSoup(page.text, 'html.parser')
